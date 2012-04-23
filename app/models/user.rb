66c7e9b5e3771aa084925e0f7b3b9df6f4d238ac
Marco Arias <mtarias@uc.cl>
2012-04-23 10:39:25 -0400

    Users verifican email, y la pagina principal funciona el Registarse :)

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,8 @@ class User < ActiveRecord::Base
   has_many :group_members
   has_many :user_events
   attr_accessible :email, :password, :facebook, :twitter
+  validates :email,  :presence => true, :uniqueness => true
+  validates :password,  :presence => true, :length => {:within => 6..40}
+  validates_format_of :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i,
+   	:message    => ' debe ser valido'
 end
