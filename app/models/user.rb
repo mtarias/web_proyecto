a6e08a86f819eccd33c21616d11c482c02ba533b
Carlos Molina <camolin3@uc.cl>
2012-05-01 14:46:54 -0400

    Edité users, de forma que las contraseñas aparezcan como passwords, y que los atributos que no estemos utilizando por ahora no aparezcan

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,10 @@ class User < ActiveRecord::Base
   has_many :groups
   has_many :group_members
   has_many :user_events
-  attr_accessible :email, :password, :facebook, :twitter
+  attr_accessible :email, :password, :password_confirmation, :facebook, :twitter
   validates :email,  :presence => true, :uniqueness => true
   validates :password,  :presence => true, :length => {:within => 6..40}
   validates_format_of :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i,
    	:message    => ' debe ser valido'
+  validates_confirmation_of :password
 end
