c9f75aa375cd6c44ffecda1b975227146cf56095
Carlos Molina <camolin3@uc.cl>
2012-05-06 13:43:24 -0400

    Redirige de la página principal a profile si es que el usuario ya inició sesión

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,9 @@ layout 'home', :except => :profile
 layout 'login', :only => :profile
 
   def index
+    if session[:user_id]
+      redirect_to profile_path
+    end
   end
 
   def login
@@ -20,16 +23,17 @@ layout 'login', :only => :profile
   		if user = User.find_by_email(params[:email])
   			@user = user
   		else
-  			redirect_to :root, :notice => "No existe usuario con ese email, weon"
+  			redirect_to :root, :notice => "No existe usuario con ese email"
   		end
   	elsif session[:user_id]
   		@user = User.find(session[:user_id])
   	else
-  		redirect_to :root, :notice => "No existe usuario con ese email, aweonao"
+  		redirect_to :root, :notice => "Inicie sesión nuevamente"
   	end
   end
 
   def logout
+    @user = nil
   	reset_session
   	redirect_to :root, :notice => "Has cerrado sesión correctamente"
   end
