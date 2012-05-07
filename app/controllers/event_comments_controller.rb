54dcf290fece1f351bd65cafd2d8bd841fa51ee7
Carlos Molina <camolin3@uc.cl>
2012-05-07 01:17:21 -0400

    Los comentarios -al fin- guardan el usuario que lo creó

--- app/controllers/event_comments_controller.rb
+++ app/controllers/event_comments_controller.rb
@@ -2,6 +2,8 @@ class EventCommentsController < ApplicationController
 	def create
 	    @event = Event.find(params[:event_id])
 	    @event_comment = @event.event_comments.create(params[:event_comment])
+	    @event_comment.user_id = session[:user_id]
+	    @event_comment.save
 	    redirect_to event_path(@event)
 	  end
 	  def destroy
