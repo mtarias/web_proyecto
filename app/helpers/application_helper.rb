520716233575e93510ac8bae951ac623abed5ee8
Carlos Molina <camolin3@uc.cl>
2012-06-03 15:23:05 -0400

    Las páginas tienen título

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
 		form.submit + link_to(name, '#', {}, :onclick => 'history.go(-1)', :class => "cancel")
 	end
 
+	def title(title)
+		content_for :title, I18n.t(title)
+	end
+
 end
