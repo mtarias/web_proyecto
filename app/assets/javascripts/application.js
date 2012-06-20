3625619a31faede53581bfa3ef40196fb9c06db4
Carlos Molina <camolin3@uc.cl>
2012-06-20 15:18:58 -0400

    Eliminé JS que no ocupamos. JS carga al final del documento, aumenta velocidad.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,7 +8,9 @@
 // the compiled file.
 //
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW. // require_tree .
+// GO AFTER THE REQUIRES BELOW.
+
+// require_tree .
 //
 //= require jquery
 //= require jquery_ujs
